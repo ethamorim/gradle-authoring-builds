d8c47f4b60825d28e8938483a830a42518e21fb7
ethamorim <ethamorim@gmail.com>
2024-07-26 15:39:09 -0300

    create a plugin and add the build to settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,6 @@
 rootProject.name = "gradle-authoring-builds"
 include("app")
 include("lib")
+includeBuild("gradle/license-plugin")
 
 println("SETTINGS FILE: This is executed during the initialization phase")
